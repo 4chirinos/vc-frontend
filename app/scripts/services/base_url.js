db20f3ab3891ec1801209bc866e00333fdb5b1c7
4rchico <archico000@gmail.com>
2016-11-20 18:02:06 -0400

    Subiendo a heroku

--- app/scripts/services/base_url.js
+++ app/scripts/services/base_url.js
@@ -8,9 +8,8 @@
  * Constant in the frontend2App.
  */
 angular.module('frontend2App')
-  .constant('baseUrl', 'https://pacific-inlet-56422.herokuapp.com/api/v1');
+  .constant('baseUrl', 'https://powerful-river-28315.herokuapp.com/api/v1');
 
 // 'http://localhost:3000/api/v1'
 
-// 'https://pacific-inlet-56422.herokuapp.com/api/v1'
-// 'https://pacific-inlet-56422.herokuapp.com/api/v1'
+// 'https://powerful-river-28315.herokuapp.com/api/v1'
