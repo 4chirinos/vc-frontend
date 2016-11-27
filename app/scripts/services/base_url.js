ac8801cf9867b1f4dc25dbd6c81ef31c175bae6e
4rchico <archico000@gmail.com>
2016-11-27 12:39:49 -0400

    Cambiando al url a donde está el API

--- app/scripts/services/base_url.js
+++ app/scripts/services/base_url.js
@@ -8,8 +8,8 @@
  * Constant in the frontend2App.
  */
 angular.module('frontend2App')
-  .constant('baseUrl', 'https://powerful-river-28315.herokuapp.com/api/v1');
+  .constant('baseUrl', 'http://localhost:3000/api/v1');
 
 // 'http://localhost:3000/api/v1'
 
-// 'https://powerful-river-28315.herokuapp.com/api/v1'
+// https://powerful-river-28315.herokuapp.com/api/v1
