2282f89d339a76d14958c4d02fbb17c16f5927bc
4rchico <archico000@gmail.com>
2017-04-08 22:24:24 -0400

    Subiendo a heroku

--- app/scripts/services/base_url.js
+++ app/scripts/services/base_url.js
@@ -9,7 +9,4 @@
  */
 angular.module('frontend2App')
   .constant('baseUrl', 'https://powerful-river-28315.herokuapp.com/api/v1');
-
-// 'http://localhost:3000/api/v1'
-
-// https://powerful-river-28315.herokuapp.com/api/v1
+  //.constant('baseUrl', 'http://localhost:3000/api/v1');
